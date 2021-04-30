cbf3db868d73f51555a7e69c9de60d82d55121ea
hui.luo <hui.luo@vhall.com>
2021-04-30 20:09:23 +0800

    55

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
-var path = require("path")
+var path = require('path')
 module.exports = {
-    root: true,
-    "extends": ["eslint:recommended", "prettier"],
-    env: {
-        browser: true
-    },
-    "plugins": ["prettier"],
-    rules: {
-        "prettier/prettier":"error", // 不符合 prettier 规则的代码，要进行错误提示（红线）
-        'no-console': 'off',
-        'semi': [2],
-        'quotes': [1, 'single']
-    }
-};
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  env: {
+    browser: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error', // 不符合 prettier 规则的代码，要进行错误提示（红线）
+    'no-console': 'off',
+    semi: [0],
+    quotes: [1, 'single'],
+  },
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 function merge() {
-  var ret = {};
+  var ret = {}
   for (var i in arguments) {
-    var m = arguments[i];
-    for (var j in m) ret[j] = m[j];
+    var m = arguments[i]
+    for (var j in m) ret[j] = m[j]
   }
-  return ret;
+  return ret
 }
 
-var ad1= "5"
+var ad1 = '5'
 
-var ada3 = 'gege';
+var ada3 = 'gege'
 console.log(merge({ a: 123 }, { b: 456 }))
-
-function f(a, b) {
-  return a + b
-}
+console.log(ada3, ad1)
